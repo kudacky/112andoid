38748a35a18b8d52400da95db7d16d7acb42abc5
kudacky <ckykuda88@gmail.com>
2024-03-29 15:00:11 +0800

    2024-03-29

--- BMI2/app/src/main/java/com/example/bmi2/MainActivity.java
+++ BMI2/app/src/main/java/com/example/bmi2/MainActivity.java
@@ -1,21 +1,16 @@
-package com.example.bmi;
+package com.example.bmi2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
-import android.graphics.ImageFormat;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
-public class MainActivity extends AppCompatActivity implements View.OnClickListener
-{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     private TextView txvShow;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,22 +18,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         txvShow = (TextView) findViewById(R.id.txvShow);
         txvShow.setTextSize(36);
-        Button btnCale = (Button) findViewById(R.id.btnCalc);
+        Button btnCalc = (Button) findViewById(R.id.btnCalc);
         Button btnClear = (Button) findViewById(R.id.btnClear);
-        btnCale.setOnClickListener(this);
+        btnCalc.setOnClickListener(this);
         btnClear.setOnClickListener(this);
     }
 
-
     @Override
-    public void onClick(View v){
+    public void onClick(View v) {
         EditText edtHeight = (EditText) findViewById(R.id.edtHeight);
         EditText edtWeight = (EditText) findViewById(R.id.edtWeight);
 
-        if(v.getId() == R.id.btnCalc) {
+        if (v.getId() == R.id.btnCalc) {
             double height = Double.parseDouble(edtHeight.getText().toString());
             double weight = Double.parseDouble(edtWeight.getText().toString());
-            double bmi = weight / Math.pow(height / 100.0, 2);
+            double bmi = weight / Math.pow(height/100.0 , 2);
             if (bmi >= 24)
                 txvShow.setTextColor(Color.RED);
             else if (bmi < 18.5)
@@ -49,18 +43,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             txvShow.setText(String.format("%.2f", bmi));
         }
         else {
-             edtHeight.setText("0");
-             edtWeight.setText("0");
-             txvShow.setText("");
+            edtHeight.setText("0");
+            edtWeight.setText("0");
+            txvShow.setText("");
         }
-
-
-
-
-
-
-
-
-
-        }
+    }
 }
